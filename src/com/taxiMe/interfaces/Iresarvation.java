d4c260d21ee8cc35e7c7425fc4f7d143f2741d35
hashi <it19184340@my.sliit.lk>
2021-05-28 21:53:20 +0530

    adding reservation  part to taxime

---
+++ src/com/taxiMe/interfaces/Iresarvation.java
@@ -0,0 +1,17 @@
+package com.taxiMe.interfaces;
+
+import java.util.ArrayList;
+
+import com.taxiMe.model.Resarvation;
+  
+
+public interface Iresarvation {
+	public ArrayList<Resarvation> getResarvationList();
+	public Resarvation getResarvation(int id); 
+	public ArrayList<Resarvation> getResarvationByUserId(int userId);
+	public ArrayList<Resarvation> getResarvationByStatus(String status);
+	public boolean addResarvation(Resarvation resarvation);
+	public boolean updateResarvation (Resarvation resarvation);
+	public boolean deleteResarvation(int id);
+	public boolean clangeResarvationStatus(int id ,String status);
+}
---
+++ src/com/taxiMe/servlet/resarvation/deleteReservation.java
@@ -0,0 +1,79 @@
+package com.taxiMe.servlet.resarvation;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.taxiMe.interfaces.Iresarvation;
+import com.taxiMe.interfaces.Ivehicle;
+import com.taxiMe.services.Sresarvation;
+import com.taxiMe.services.Svehicle;
+
+ 
+/**
+ * Servlet implementation class deleteReservation
+ */ 
+public class deleteReservation extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public deleteReservation() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id =  Integer.parseInt(request.getParameter("id"));
+		 
+		String vid =  request.getParameter("vid"); 
+		// create boolean variable for get out put of query
+		boolean isTrue;
+		
+		Iresarvation service = new Sresarvation();
+		Ivehicle vehicleservice = new Svehicle();
+		
+		//call function
+		try {
+			isTrue = service.deleteResarvation(id) ; 
+			if(request.getParameter("vid") != "null") {
+				vehicleservice.changeVehicleStatus( "not-assigned",request.getParameter("vid"));
+			}
+			
+			if(isTrue == true) {
+				
+				RequestDispatcher dispatcher = request.getRequestDispatcher("resarvationManegment.jsp");
+				dispatcher.forward(request, response);
+				
+			}
+			else { 
+				 
+				 
+				RequestDispatcher dispatcher = request.getRequestDispatcher("resarvationManegment.jsp");
+				dispatcher.forward(request, response);
+				
+			}
+		}
+		catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
